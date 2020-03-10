0714cb08f0ff5a53d04fdeef18fbe3f3d1833d96
jaewon0913 <jaewon9937@gmail.com>
2020-03-10 18:42:14 +0900

    oauth study start

---
+++ src/main/java/com/ouath/mvc/BootOauthStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ouath.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOauthStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOauthStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ouath/mvc/BootOauthStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ouath.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootOauthStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
