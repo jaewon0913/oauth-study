38dc6ab96bbd632fdcdc2b6da6476d5542dccd2e
jaewon0913 <jaewon9937@gmail.com>
2020-03-12 12:28:54 +0900

    oauth study start

--- src/main/java/com/ouath/mvc/SpringbootOauthStudyApplication.java
+++ src/main/java/com/ouath/mvc/SpringbootOauthStudyApplication.java
@@ -2,14 +2,20 @@ package com.ouath.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages="com.ouath.mvc")
-public class SpringbootOauthStudyApplication {
+public class SpringbootOauthStudyApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootOauthStudyApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootOauthStudyApplication.class, args);
 	}
-
 }
